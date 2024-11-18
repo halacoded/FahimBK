9d4c706c7ec5fb1854902f81290c9540fe4b0740
halacoded <adult.hala222@outlook.com>
2024-11-18 17:17:53 +0300

    Setup

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
+const passport = require("passport");
+const path = require("path");
+const {
+  localStrategy,
+  jwtStrategy,
+  JwtStrategy,
+} = require("./middleware/passport.js");
 const connectDB = require("./database.js");
-
+const notFoundHandler = require("./middleware/notFoundHandler");
+const errorHandler = require("./middleware/errorHandler.js");
 //init
 const PORT = process.env.PORT || 80000;
 dotenv.config();
@@ -15,15 +23,20 @@ const app = express();
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
+passport.use("local", localStrategy);
+passport.use("jwt", JwtStrategy);
 
 // MongoDB connection
 connectDB();
 
 // Routes
-
+app.use("/media", express.static(path.join(__dirname, "media")));
 // Not Found Handling middleware
 
+app.use(notFoundHandler);
+
 // Error handling middleware
+app.use(errorHandler);
 
 // Start server
 app.listen(PORT, () => {
